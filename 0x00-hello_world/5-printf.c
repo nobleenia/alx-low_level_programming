32a3ee2a64b04334300ae1555cebdd28b5c2ea80
Noble <neluwah@gmail.com>
2023-06-15 07:25:01 -0700

    5-print.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Program starting point
+ *
+ *return - 0 (indicating successful)
+ */
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art,");
+  return 0;
+}
