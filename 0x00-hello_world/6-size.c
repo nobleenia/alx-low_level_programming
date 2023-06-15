f34b01ff718b5c161b3c8d31bedd1f4cba426e3a
Noble <neluwah@gmail.com>
2023-06-15 07:55:55 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,15 +8,8 @@ int main(void)
 {
 printf("Size of char: %lu byte(s)\n", sizeof(char));
 printf("Size of int: %lu byte(s)\n", sizeof(int));
-printf("Size of float: %lu byte(s)\n", sizeof(float));
-printf("Size of double: %lu byte(s)\n", sizeof(double));
 printf("Size of long: %lu byte(s)\n", sizeof(long));
 printf("Size of long long: %lu byte(s)\n", sizeof(long long));
-printf("Size of pointer: %lu byte(s)\n", sizeof(void*));
-printf("Size of a char: 1 byte(s)");
-printf("Size of an int: 4 byte(s)");
-printf("Size of a long int: 4 byte(s)");
-printf("Size of a long long int: 8 byte(s)");
-printf("Size of a float: 4 byte(s)");
+printf("Size of float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
