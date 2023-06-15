30861676b90a75a7f95d6dad4f809914bcf183b8
Noble <neluwah@gmail.com>
2023-06-15 08:49:03 -0700

    6-size.c~

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of char: %lu byte(s)\n", sizeof(char));
-printf("Size of int: %lu byte(s)\n", sizeof(int));
-printf("Size of long: %lu byte(s)\n", sizeof(long));
-printf("Size of long long: %lu byte(s)\n", sizeof(long long));
-printf("Size of float: %lu byte(s)\n", sizeof(float));
+printf("Size of char: %d byte(s)\n", sizeof(char));
+printf("Size of int: %d byte(s)\n", sizeof(int));
+printf("Size of long int: %d byte(s)\n", sizeof(long int));
+printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of float: %d byte(s)\n", sizeof(float));
 return (0);
 }
