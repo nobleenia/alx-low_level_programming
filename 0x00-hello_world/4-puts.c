0df9d2a05680ccb783cc8a5549120bac038608b0
Noble <neluwah@gmail.com>
2023-06-15 07:01:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main:  Entry point of the program
+ * main - Entry point of the program
  *
- * Return - Always 0 SUCCESS
- */
+ * Return - Always 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
