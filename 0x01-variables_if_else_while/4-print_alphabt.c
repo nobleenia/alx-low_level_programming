600cfe23b0184f4c20cf7c54ea2d33697734da85
Noble <neluwah@gmail.com>
2023-06-16 06:57:46 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Starting point of the program
+ *
+ * Return: 0 (indication of success)
+ */
+int main(void)
+{
+char lower_cases = 'a';
+
+while (lower_cases <= 'z' && lower_cases != 'q' || 'e')
+{
+putchar(lower_cases);
+lower_cases++;
+}
+putchar('\n');
+return (0)
+}
