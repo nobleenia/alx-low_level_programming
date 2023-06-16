19f3046100965c0c749cd9991d7d503cc91c5586
Noble <neluwah@gmail.com>
2023-06-16 07:30:45 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of the program
+ *
+ * Return: 0 (indicating successful completion)
+ */
+
+int main(void)
+{
+int single_digit = 0;
+while (single_digit <= 9)
+{
+printf(single_digit);
+}
+print("\n")
+return (0);
+}
