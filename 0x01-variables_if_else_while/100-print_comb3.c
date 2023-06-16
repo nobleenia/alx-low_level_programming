58e9da332d6296a188228604c762a8f6332942b6
Noble <neluwah@gmail.com>
2023-06-16 10:49:59 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+int first_digit = 0;
+int second_digit = 0;
+
+while (second_digit <= 9)
+{
+while (first_digit <= 8)
+{
+if (second_digit > first digit)
+putchar('0' + first_digit);
+putchar('0' + second_digit);
+}
+if (second_digit != 9 || first_digit != 8)
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
