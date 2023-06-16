68cd9bb3a31f731fadd6076687ef03382b5e1a00
Noble <neluwah@gmail.com>
2023-06-16 05:51:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Starting point of program
+ *
+ * Return: 0 (indicating successful)
+ */
+int main(void)
+{
+char lower_case = "a";
+while (lower_cases <= "z")
+    {
+    putchar(lower_case);
+    lower_case ++;
+    }
+putchar("\n");
+return (0);
+}
