ac81ccb5dfb2badb12dcc5f7027183a137cd0761
Noble <neluwah@gmail.com>
2023-06-16 07:01:47 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,11 @@ int main(void)
 {
 char lower_cases = 'a';
 
-while (lower_cases <= 'z' && lower_cases != 'q' || 'e')
+ while (lower_cases <= 'z' '&&' lower_cases != 'q' || 'e')
 {
 putchar(lower_cases);
 lower_cases++;
 }
 putchar('\n');
-return (0)
+ return (0);
 }
