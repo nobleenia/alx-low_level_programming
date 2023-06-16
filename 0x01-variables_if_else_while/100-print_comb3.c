0b1840d478ee42527b9fd7d67f17e6ea935901e3
Noble <neluwah@gmail.com>
2023-06-16 11:46:56 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,13 @@ int second_digit;
 for (first_digit = 0; first_digit <= 8; first_digit++)
 {
 for (second_digit = 0; second_digit <= 9; second_digit++)
-if (second_digit > first_digit)
+while (second_digit > first_digit)
 {
 putchar('0' + first_digit);
 putchar('0' + second_digit);
-}
+
 if (first_digit != 8 && second_digit != 9)
+{
 putchar(',');
 putchar(' ');
 }
