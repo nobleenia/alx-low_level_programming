d2400869bcd090b41f3b0c60055aa0b983f1d14d
Noble <neluwah@gmail.com>
2023-06-16 08:34:15 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int main(void)
+{
+char lower_cases = 'z';
+
+while (lower_cases >= 'a')
+{
+putchar(lower_cases);
+lower_cases--;
+}
+putchar('\n');
+return (0);
+}
