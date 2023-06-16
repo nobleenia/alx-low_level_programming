31cb57fa880045359a64ee6d4fead92a8a8bc532
Noble <neluwah@gmail.com>
2023-06-16 06:33:15 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,12 +11,16 @@ char lower_cases = 'a';
 char upper_cases = 'A';
 
 while (lower_cases <= 'z')
+{
 putchar(lower_cases);
 lower_cases++;
+}
 
 while (upper_cases <= 'Z')
+{
 putchar(upper_cases);
 lower_cases++;
+}
 
 putchar('\n');
 return (0);
