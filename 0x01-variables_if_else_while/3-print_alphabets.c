3ee1d61e5a3b231fa82ddbd180c28011061c5a1e
Noble <neluwah@gmail.com>
2023-06-16 06:21:02 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: 0 (indication of success)
+ */
+int main(void)
+{
+char lower_cases = 'a';
+char upper_cases = 'A';
+
+while (lower_cases < 'z')
+putchar(lower_cases);
+lower_cases++;
+
+while (ipper_cases < 'Z')
+putchar(upper_cases);
+lower_cases++;
+
+putchar('\n');
+return (0);
+}
