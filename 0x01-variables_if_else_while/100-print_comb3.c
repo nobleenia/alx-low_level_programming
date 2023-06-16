2712dfadd56a6958b6a6e281dd9a4cf464f3df74
Noble <neluwah@gmail.com>
2023-06-16 11:43:55 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,25 +2,24 @@
 
 /**
  * main - input of the code
- * while loop for the first digit
+ * for loop for the first digit
  * for loop that iterates the second digit
  * Return: 0 (successful)
  */
 int main(void)
 {
-int first_digit = 0;
-int i;
+int first_digit;
+int second_digit;
 
-while (first_digit <= 8)
+for (first_digit = 0; first_digit <= 8; first_digit++)
 {
-for (i = 0; i <= 9; i++)
-if (i > first_digit)
+for (second_digit = 0; second_digit <= 9; second_digit++)
+if (second_digit > first_digit)
 {
 putchar('0' + first_digit);
-putchar('0' + i);
-first_digit++;
+putchar('0' + second_digit);
 }
-if (first_digit != 8 && i != 9)
+if (first_digit != 8 && second_digit != 9)
 putchar(',');
 putchar(' ');
 }
