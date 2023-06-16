a20be3066c3d7a25b90eebb7f5e3099aada3db2a
Noble <neluwah@gmail.com>
2023-06-16 09:30:51 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+int digits = 0;
+
+while (digits <= 9)
+{
+putchar('0' + digits, ',');
+digits++;
+}
+
+putchar('\n');
+return (0);
+}
