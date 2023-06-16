6ededa4c29bcffd865689556d92d0bbf76a17030
Noble <neluwah@gmail.com>
2023-06-16 16:23:04 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**                                                                                                                 
+ * main - input of the code                                                                                         
+ * for loop for the first digit                                                                                     
+ * for loop that iterates the second digit                                                                          
+ * Return: 0 (successful)                                                                                           
+ */
+int main(void)
+{
+int first_digit, second_digit, third_digit;
+
+for (first_digit = 0; first_digit <= 7; first_digit++)
+{
+for (second_digit = 0; second_digit <= 8; second_digit++)
+{
+for (third_digit = 0; third_digit <= 9; third_digit++)
+{
+if (second_digit > first_digit)
+{
+putchar('0' + first_digit);
+putchar('0' + second_digit);
+putchar('0' + third_digit);
+if (first_digit != 7 || second_digit != 8 || third_digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
