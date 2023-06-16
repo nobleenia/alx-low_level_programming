c76dd57c62f899d1f0511e6cd8b0ca5cdf084953
Noble <neluwah@gmail.com>
2023-06-16 07:56:22 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+int main(void)
+{
+int single_digit = 0;
+while (singlr_digit <= 9)
+{
+putchar('%i', single_digit);
+single_digit++;
+}
+putchar('\n');
+return (0);
+}
