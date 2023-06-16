bde33fef038cd783ec227069aa0c65171a88d77c
Noble <neluwah@gmail.com>
2023-06-16 05:59:02 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,10 @@ int main(void)
 {
 char lower_case = 'a';
 while (lower_case <= 'z')
-    {
-    putchar(lower_case);
-    lower_case ++;
-    }
+{
+putchar(lower_case);
+lower_case ++;
+}
 putchar('\n');
 return (0);
 }
