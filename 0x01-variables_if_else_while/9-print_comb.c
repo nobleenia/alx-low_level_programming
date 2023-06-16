661703b9c0bd2915357884101ef77e1c67baa289
Noble <neluwah@gmail.com>
2023-06-16 09:46:42 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
+ * main - Program starts here
  *
+ * while loop to iterate within the range
+ * if loop to define the exception
  *
- *
- *
- *
+ * Return: 0 (indicating succesful running)
  */
 int main(void)
 {
