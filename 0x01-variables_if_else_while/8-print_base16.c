cb2075f2e7a51a160c6cbaf1cdc71d335ff82a89
Noble <neluwah@gmail.com>
2023-06-16 09:08:54 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <sttdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: 0 (indicate successful running)
+ */
+int main (void)
+{
+int hex_numbers = 0;
+char hex_alphabets = 'a';
+
+while (hex_numbers <= 9)
+{
+putchar(hex_numbers);
+hex_numbers++;
+}
+while (hex_alphabets <= 'f')
+{
+putchar(hex_alphabets);
+hex_alphabets++;
+}
+putchar('\n');
+return (0);
+}
