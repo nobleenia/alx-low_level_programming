c7d47a9723d88b6a50b2ffd17665f517f6967a5a
Noble <neluwah@gmail.com>
2023-06-16 11:05:03 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,15 +18,13 @@ while (first_digit <= 8)
 if (second_digit > first_digit)
 putchar('0' + first_digit);
 putchar('0' + second_digit);
-second_digit++;
 first_digit++;
 }
 if (second_digit != 9 || first_digit != 8)
 putchar(',');
 putchar(' ');
-second_digit++;
-first_digit++;
 }
+second_digit++;
 putchar('\n');
 return (0);
 }
