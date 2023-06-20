58eb98cb28120f82e0f9d2a6f0c7528ce38d3aa9
Noble <neluwah@gmail.com>
2023-06-20 15:14:54 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void print_to_98(int n)
+{
+int first_print = n;
+int last_print = 98;
+if (last_print > first_print)
+{
+_putchar('0' + first_print);
+first_print++;
+}
+else if (last_print < first_print)
+{
+_putchar('0' + first_print);
+first_print-;-
+}
+else
+{
+_putchar('0'+ last_print);
+}
+}
