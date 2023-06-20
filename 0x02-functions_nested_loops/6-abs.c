4ba5366a58d8f65d738dccf817e18cab24872401
Noble <neluwah@gmail.com>
2023-06-20 07:13:10 -0700

    int _abs(int)

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - entry point of the function
+ * @int: the number to be absoluted
+ * Return: absolute_val show success
+ */
+int _abs(int)
+{
+int absolute_val;
+if (int < 0);
+{
+absolute_val = int * -1;
+}
+return (absolute_val);
+}
