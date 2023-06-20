64582325ba0959c9bff6d41d57ffef412403afa0
Noble <neluwah@gmail.com>
2023-06-20 05:29:09 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,16 +7,14 @@
 int _islower(int c)
 {
 int lower_case = 'a';
-int classifier;
+int classifier = 0;
 while (lower_case <= 'z')
 {
 if (lower_case == c)
 {
 classifier = 1;
-}
-else
-classifier = 0;
-}
 lower_case++;
+}
+}
 return (int classifier);
 }
