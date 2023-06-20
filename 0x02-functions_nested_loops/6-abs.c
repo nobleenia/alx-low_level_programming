09e53edb96c12a45c32c4dad91ca8b8a0ca09682
Noble <neluwah@gmail.com>
2023-06-20 07:37:52 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,14 +3,11 @@
 /**
  * _abs - entry point of the function
  * @d: the number to be absoluted
- * Return: absolute_val show success
+ * Return: value of d * -1 to show success
  */
 int _abs(int d)
 {
-int absolute_val;
-if (d < 0);
+if (d <= 0);
 {
-absolute_val = d * -1;
-}
-return (absolute_val);
+return (d * -1);
 }
