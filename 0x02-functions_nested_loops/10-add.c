1a934dab4e5aed773f20ae427a5967d7936fc9d8
Noble <neluwah@gmail.com>
2023-06-20 14:29:23 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - the start of the function
+ *
+ * Return: result of the sum of the input
+ */
+int add(int a, int b)
+{
+int result = a + b;
+
+return (result);
+}
