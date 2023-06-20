dbfef20fe7567fbf26bea0d91716f0e4e0c98b42
Noble <neluwah@gmail.com>
2023-06-20 01:55:15 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of program
+ *
+ * Return: 1 (Successful return for an int)
+ */
+int main(void)
+{
+void print_alphabet(void)
+{
+int lower_case = 'a';
+
+while (lower_case <= 'z')
+{
+putchar("lower_case");
+lower_case++;
+}
+putchar('\n');
+return (1);
+}
+}
