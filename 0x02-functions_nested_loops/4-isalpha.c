c9b722809c3dd4e9e8574cc628f9220833e4b942
Noble <neluwah@gmail.com>
2023-06-20 06:31:06 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,10 +10,10 @@ int _isalpha(int c)
 int lower_case, upper_case;
 int alphabet = 0;
 
-int lower_case = 'a';
+lower_case = 'a';
 while (lower_case <= 'z')
 {
-int upper_case = 'A';
+upper_case = 'A';
 while (upper_case <= 'Z')
 {
 if (lower_case == c || upper_case == c)
