ba2f2b32294088bfe31bece8c31c07cf053b1aa8
Noble <neluwah@gmail.com>
2023-06-20 14:19:22 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,46 +9,30 @@ void times_table(void)
 {
 int  row_val, col_val, product_val;
 
-row_val = 0;
-while (row_val < 10)
+for (row_val = 0; row_val <= 9; row_val++)
 {
-col_val = 0;
-while (col_val < 10)
+for (col_val = 0; col_val <= 9; col_val++)
 {
 product_val = row_val * col_val;
-if (col_val != 9)
+if (col_val == 0)
 {
-if ((product_val / 10) == 0)
+_putchar('0' + product_val);
+}
+if (product_val < 10  && col_val != 0)
 {
-_putchar(' ');
-_putchar(product_val + '0');
 _putchar(',');
 _putchar(' ');
+_putchar(' ');
+_putchar('0' + product_val);
 }
-else
+else if (product_val >= 10
 {
-_putchar((product_val / 10) + '0');
-_putchar((product_val % 10) + '0');
 _putchar(',');
 _putchar(' ');
-}
-}
-else
-{
-if ((product_val / 10) == 0)
-{
-_putchar(' ');
-_putchar(product_val + '0');
-}
-else
-{
 _putchar((product_val / 10) + '0');
 _putchar((product_val % 10) + '0');
 }
 }
-col_val++;
-}
-row_val++;
 _putchar('\n');
 }
 }
