e51ace90b5164109d5cb71cfe816cf8b2f8bd754
Noble <neluwah@gmail.com>
2023-06-20 10:52:01 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,12 +15,14 @@ while (hrs <= 23)
 mins = 0; 
 while (mins <= 59)
 {
-_putchar('0' + hrs);
+_putchar('0' + (hrs / 10));
+_putchar('0' + (hrs % 10));
 _putchar(':');
-_putchar('0' + mins);
+_putchar('0' + mins / 10);
+_putchar('0' + mins % 10);
 _putchar('\n');
 mins++;
 }
- hrs++;
+hrs++;
 }
 }
