1d2df25255600bc8ab8b269302de0115ab7b2726
Noble <neluwah@gmail.com>
2023-06-21 01:23:24 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,15 +9,15 @@ int main(void)
 {
 
 int sum = 0;
-
-int i;
-for (i = 0; i < 1024; i++)
+int i = 0;
+while (i < 1024)
 {
 if ((i % 3) == 0 || (i % 5) == 0)
 {
-sum = sum + i;	
+sum = sum + i;
+i++;
 }
 printf("%d\n", sum);
-}
+
 return (0);
 }
