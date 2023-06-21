f2406044e622b876a80d5ec161b90651948c93ff
Noble <neluwah@gmail.com>
2023-06-21 04:35:54 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,33 +7,22 @@
  */
 int main(void)
 {
-unsigned long n, prev_1, prev_2, prev_3, post_1, post_2, post_3, len;
+unsigned long prev_1, prev_2, post_1, post_2, dis;
+unsigned int n;
 
 prev_1 = 1;
 prev_2 = 2;
-len = 1000000000;
 
-printf("%lu", prev_1);
+printf("%lu, %lu", prev_1, prev_2);
 
-for (n = 1; n < 91; n++)
+for (n = 3; n < 98; n++)
 {
+post_1 = prev_1 + prev_2;
 printf(", %lu", post_1);
-post_1 = post_1 + prev_1;
-prev_1 = post_1 - prev_1;
-}
-prev_2 = (prev_1 / len);
-prev_3 = (prev_1 % len);
-post_2 = (post_1 / len);
-post_3 = (post_1 % len);
 
-for (n = 92; n < 99; n++);
-{
-printf(", %lu", post_2 + (post_3 / len));
-printf("%lu", post_3 % len);
-post_2 = post_2 + prev_2;
-prev_2 = post_2 - prev_2;
-post_3 = post_3 + prev_3;
-prev_3 = post_3 - prev_3;
+dis = prev_1;
+prev_1 = prev_2;
+prev_2 = post_1;
 }
 
 printf("\n");
