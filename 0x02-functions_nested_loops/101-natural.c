c4f9c56c01f9471c0cbf1054fa158d6ac54c2a91
Noble <neluwah@gmail.com>
2023-06-21 00:57:21 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ *
+ *
+ */
+int main(void)
+{
+int a = 1024;
+int counter = 0;
+int i;
+for (i = 0; i < a; i++)
+{
+if (i % 3 = 0 || i % 5 = 0)
+{
+counter = counter + 1;	
+}
+printf("%d\n", counter);
+}
+}
