1b7f9bf050bfc149999a3c6c9cc9407f89c00a5d
Noble <neluwah@gmail.com>
2023-06-21 12:48:46 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,24 +7,37 @@
  */
 int main(void)
 {
-unsigned long prev_1, prev_2, post_1;
+unsigned long prev_1, prev_2, post_1, post_2, carry_1, carry_2, carry_3, limit;
 int n;
 
-prev_1 = 1;
-prev_2 = 2;
-post_1 = 0;
+prev_1 = 0;
+prev_2 = 0;
+post_1 = 1;
+post_2 = 2;
+limit = 10000000000;
 
-printf("%lu, %lu", prev_1, prev_2);
-
-for (n = 0; n <= 97; n++)
+printf("%lu, %lu", post_1, post_2);
+for (count = 2; count < 98; count++)
 {
-post_1 = prev_1 + prev_2;
-printf(", %lu", post_1);
-prev_1 = prev_2;
-prev_2 = post_1;
+
+if (post_1 + post_2 > limit || prev_2 > 0 || prev_1 > 0)
+{
+carry_1 = (post_1 + post_2) / limit;
+carry_2 = (post_1 + post_2) % limit;
+carry_3 = prev_1 + prev_2 + carry_1;
+prev_1 = prev_2, prev_2 = carry_3;
+post_1 = post_2, post_2 = carry_2;
+printf("%lu%010lu", prev_2, post_2);
 }
-
+else
+{
+carry_2 = post_1 + post_2;
+prev_1 = post_2, post_2 = carry_2;
+printf("%lu", post_2);
+}
+if (count != 97)
+printf(", ");
+}
 printf("\n");
-
 return (0);
 }
