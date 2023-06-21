5a78bafb8f867189fca6b4872c1df7d98ff8a3cd
Noble <neluwah@gmail.com>
2023-06-21 02:00:11 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main- initializes the program
+ * the fibonacci algorithms
+ * Return: 0 successful completion
+ */
+int main(void)
+{
+int f[50];
+int n;
+
+f[0] = 1;
+f[1] = 2;
+
+for (n = 2; n <= 49; n++)
+{
+f[n] = f[n - 1] + f[n - 2];
+}
+
+for (n = 0; n <= 49; n++)
+{
+printf("%d", f[n]);
+}
+printf("%d\n", f[49]);
+
+return (0);
+}
