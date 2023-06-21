97532af82611f69c3a53338ba0c76475fd215a20
Noble <neluwah@gmail.com>
2023-06-21 02:42:57 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+* main- initializes the program
+* the fibonacci algorithms
+* Return: 0 successful completion
+*/
+int main(void)
+{
+unsigned long fst, sec, sum_val, sum;
+int n;
+
+fst = 0;
+sec = 0;
+sum = 0;
+
+for (n = 1; n <= 34; n++)
+{
+if (sec < 4000000 && (sec % 2) == 0)
+{
+sum = sum + sec
+}
+sum_val = fst + sec;
+fst = sec;
+sec = sum_val;
+}
+printf("%lu\n", sum);
+return (0);
+}
