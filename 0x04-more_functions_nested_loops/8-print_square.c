700a2e74eedb47f694f3c138902793d88b0ada66
Noble <neluwah@gmail.com>
2023-06-22 13:28:02 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ */
+void print_square(int size)
+{
+int b, l;
+if b > 0
+{
+for (b = 0; b <= size; b++)
+{
+for (l = 0; l <= size; l++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
