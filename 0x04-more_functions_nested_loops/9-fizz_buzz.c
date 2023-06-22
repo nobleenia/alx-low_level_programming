242f678a6125953e78834747da5c3630ee66d5da
Noble <neluwah@gmail.com>
2023-06-22 12:39:14 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,11 +14,11 @@ if ((a % 3 == 0) && (a % 5 == 0))
 {
 printf("FizzBuzz ");
 }
-else if (a % 3 == 0)
+else if ((a % 3 == 0) && (a % 5 != 0))
 {
 printf("Fizz ");
 }
- else if (a % 5 == 0)
+else if ((a % 5 == 0) && (a % 3 != 0))
 {
 printf("Buzz ");
 }
