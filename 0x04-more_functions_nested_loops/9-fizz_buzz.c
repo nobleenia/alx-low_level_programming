7c5eabbaabbc4f6872eaef7d1c9d66f7a5456430
Noble <neluwah@gmail.com>
2023-06-22 12:54:47 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int a;
-for (a = 1; a <= 100; a++)
+for (a = 1; a < 100; a++)
 {
 if ((a % 3 == 0) && (a % 5 == 0))
 {
@@ -22,15 +22,12 @@ else if (a % 5 == 0)
 {
 printf("Buzz ");
 }
-else if (a == 100)
-{
-printf("Buzz");
-}
 else
 {
 printf("%d ", a);
 }
 }
+printf("Buzz");
 printf("\n");
 return (0);
 }
