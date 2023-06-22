5f1a82500f7b08670d905f47a42c9200007f72de
Noble <neluwah@gmail.com>
2023-06-22 10:21:23 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * void print_line(int n)
+ * @n: n is the number of times the character is printed
+ *
+ */
+void print_line(int n)
+{
+int i;
+if (n > 0);
+{
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
+_putchar('\n');
+}
