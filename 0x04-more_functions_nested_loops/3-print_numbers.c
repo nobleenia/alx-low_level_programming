3e5c4129ea59b900f1760b947020fb82bf14489f
Noble <neluwah@gmail.com>
2023-06-22 08:54:07 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
+ * print_numbers - starting point of function
  *
- *
- *
+ * digit - the digits to be printed
  */
 void print_numbers(void)
 {
 
 char digit = '0';
-{
+
 while (digit <= '9')
+{
 putchar(digit);
 digit++;
 }
