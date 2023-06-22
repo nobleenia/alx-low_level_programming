755ecef36c378d0ee0ba6b3fc22c98ccb6eeefec
Noble <neluwah@gmail.com>
2023-06-22 09:04:27 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - the start of function
+ *
+ * digit - the characters being printed
+ */
+void print_most_numbers(void)
+{
+char digit = '0';
+while (digit <= '9')
+{
+if (digit != '2' || digit != '4')
+_putchar(digit);
+digit++;
+}
+_putchar('\n');
+}
