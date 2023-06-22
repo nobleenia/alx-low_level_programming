6bb78682d2c75a74dabd8fe5b0cf63b688c42fef
Noble <neluwah@gmail.com>
2023-06-22 08:26:33 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - writes c to std-output
+ * @c: the digit to be checked
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+  if (c >= 0 && cc < 10)
+    return (1);
+  else
+    return (0);
+}
