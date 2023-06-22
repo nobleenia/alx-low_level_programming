fca1875596faa505fca0b10266c275b9f1e1d034
Noble <neluwah@gmail.com>
2023-06-22 09:12:08 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,11 @@ void print_most_numbers(void)
 char digit = '0';
 while (digit <= '9')
 {
-  if ((digit != '2') || (digit != '4'))
+if ((digit != '2') || (digit != '4'))
 {
 _putchar(digit);
-digit++;
 }
+digit++;
 }
 _putchar('\n');
 }
