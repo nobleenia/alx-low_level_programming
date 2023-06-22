070197be501ff4d320d67a00814698ac59b76a83
Noble <neluwah@gmail.com>
2023-06-22 13:38:53 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,9 +9,9 @@ void print_square(int size)
 int b, l;
  if (size > 0)
 {
-for (b = 0; b <= size; b++)
+for (b = 0; b < size; b++)
 {
-for (l = 0; l <= size; l++)
+for (l = 0; l < size; l++)
 {
 _putchar('#');
 }
