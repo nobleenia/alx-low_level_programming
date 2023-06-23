1e116165eab5d257af6288b1f99af6b2e3c78301
Noble <neluwah@gmail.com>
2023-06-23 00:45:44 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
 
 /**
- * main - Starting point of program
+ * main - starting point of the program
  *
- * Return: 0 successful
+ * Return: 0 (successfuly completed)
  */
 int main(void)
 {
-unsigned long prev_1, prev_2, post_1, post_2, carry_1, carry_2, carry_3, limit;
+unsigned int prev_1, prev_2, post_1, post_2, carry_1, carry_2, carry_3;
 int n;
 
 prev_1 = 0;
 prev_2 = 0;
 post_1 = 1;
 post_2 = 2;
-limit = 1000000000;
 
-printf("%lu, %lu", post_1, post_2);
+printf("%u, %u", post_1, post_2);
 for (n = 3; n <= 98; n++)
 {
-
-if (post_1 + post_2 > limit || prev_2 > 0 || prev_1 > 0)
+if (prev_2 > 0 || prev_1 > 0)
 {
-carry_1 = (post_1 + post_2) / limit;
-carry_2 = (post_1 + post_2) % limit;
+carry_1 = (post_1 + post_2) / 1000000000;
+carry_2 = (post_1 + post_2) % 1000000000;
 carry_3 = prev_1 + prev_2 + carry_1;
-prev_1 = prev_2, prev_2 = carry_3;
-post_1 = post_2, post_2 = carry_2;
-printf("%lu, %lu", prev_2, post_2);
+prev_1 = prev_2;
+prev_2 = carry_3;
+post_1 = post_2;
+post_2 = carry_2;
+printf(", %u", carry_2);
 }
 else
 {
 carry_2 = post_1 + post_2;
-prev_1 = post_2, post_2 = carry_2;
-printf("%lu", post_2);
+prev_1 = post_2;
+post_2 = carry_2;
+printf(", %u", carry_2);
 }
-if (n != 97)
-printf(", ");
 }
 printf("\n");
 return (0);
