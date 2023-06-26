33465a133dd4c0166751324a2d857d362439075c
Noble <neluwah@gmail.com>
2023-06-26 09:35:52 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -21,7 +21,7 @@ for (i = 0; i < 100; i++)
 pass[i] = rand() % 78;
 sum += (pass[i] + '0');
 putchar(pass[i] + '0');
-if ((2772 - sum) - '0' < 78);
+if ((2772 - sum) - '0' < 78)
 {
 n = 2772 - sum - '0';
 sum += n;
