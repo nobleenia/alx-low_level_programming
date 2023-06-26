3ad5c7cce662a0f6653717c09eab96c576cda4dd
Noble <neluwah@gmail.com>
2023-06-26 06:10:58 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints the strings followed by a new line
+ * @str: the strings to be printed
+ *
+ */
+void puts2(char *str)
+{
+int c;
+int len = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+for (c = 0; c < len; c++)
+{
+_putchar (str[c]);
+_putchar ('\n');
+}
+}
