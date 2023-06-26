26154f6c96c2939ec27eae116d446712d943203e
Noble <neluwah@gmail.com>
2023-06-26 03:11:06 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns length of a string
+ * @s: the character to be counted
+ * Return: the value to be returned
+ */
+int _strlen(char *s)
+{
+int len;
+len = strlen(*s);
+return (len);
+}
