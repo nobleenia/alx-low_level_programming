6919ab0e1d5493f7814c8f075bd804569df9b710
Noble <neluwah@gmail.com>
2023-06-26 04:05:21 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
+ * print_rev - prints the input characters in reverse
  *
- *
- *
+ * @s: the characters to be reversed
  */
 void print_rev(char *s)
 {
@@ -13,7 +13,7 @@ while (s[len] != '\0')
 {
 len++;
 }
- for (p = len - 1; p >= 0; p--)
+for (p = len - 1; p >= 0; p--)
 {
 _putchar(s[p]);
 }
