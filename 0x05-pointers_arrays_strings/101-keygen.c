3b309dbab5973657ed75d4b0cc1933c690b6c6a1
Noble <neluwah@gmail.com>
2023-06-26 09:34:28 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 /**
@@ -8,16 +10,17 @@
 int main(void)
 {
 int pass[100];
-int sum, n;
+int sum = 0;
+int i, n;
 
 sum = 0;
 srand(time(NULL));
 
 for (i = 0; i < 100; i++)
 {
-pass[i] = rand () % 78;
+pass[i] = rand() % 78;
 sum += (pass[i] + '0');
-putcahr(pass[i] + '0');
+putchar(pass[i] + '0');
 if ((2772 - sum) - '0' < 78);
 {
 n = 2772 - sum - '0';
@@ -27,5 +30,6 @@ break;
 	
 }
 }
+putchar('\n');
 return (0);
 }
