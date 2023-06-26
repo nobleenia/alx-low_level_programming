74570e7e7518d5a4f7a8d37e8228a2730daffb21
Noble <neluwah@gmail.com>
2023-06-26 06:21:37 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* puts_half - prints the strings halfway followed by a new line
+* @str: the strings to be printed
+*/
+void puts_half(char *str)
+{
+int c;
+int len = 0;
+while (str[len] != '\0')
+{
+len++;
+}
+for (c = 0; c <= (len / 2); c++)
+{
+_putchar (str[c]);
+}
+_putchar ('\n');
+}
