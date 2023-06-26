d76af7e895b3e4a9080600ec4ba6372e3ebd9f05
Noble <neluwah@gmail.com>
2023-06-26 10:40:57 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,32 +8,36 @@
  */
 int _atoi(char *s)
 {
-int sign = 1;
-int digit = 0;
-int n = 0;
+int i, d, n, len, f, digit;
 
-if (s[n] == '-')
-{
-sign = -1;
-n++;
-}
+i = 0;
+d = 0;
+n = 0;
+len = 0;
+digit = 0;
 
-while (s[n] != '\0')
+while (s[len] != '\0')
+len++;
+
+while (i < len && f == 0)
 {
-if (s[n] >= '0' && s[n] <= '9')
+if (s[i] == '-')
+++d;
+
+if (s[i] >= '0' && s[i] <= '9')
 {
-int ct_digit = s[n] - '0';
-digit = digit * 10 + ct_digit;
- if ((digit > INT_MAX / 10) || ((digit == INT_MAX / 10) && (ct_digit > INT_MAX % 10)))
-{
-return ((sign == -1) ? INT_MIN : INT_MAX);
-}
-}
-else
-{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
 break;
+f = 0;
 }
-n++;
+i++;
 }
-return (digit * sign);
+if (f == 0)
+return (0);
+return (n);
 }
