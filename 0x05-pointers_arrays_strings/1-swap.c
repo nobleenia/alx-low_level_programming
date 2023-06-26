1c25ec38c7910b924c6513fbbfe38b234ab70915
Noble <neluwah@gmail.com>
2023-06-26 02:59:51 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int - program that swaps values between two integers
- * @a - the first of two integers
- * @b - the second of the two integrs
+ * @a: the first of two integers
+ * @b: the second of the two integrs
  */
 void swap_int(int *a, int *b)
 {
