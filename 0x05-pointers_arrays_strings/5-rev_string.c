d7ebf4d810f42b983335c585d8b2f4c1a0dc0782
Noble <neluwah@gmail.com>
2023-06-26 05:18:28 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,14 +9,16 @@ void rev_string(char *s)
 {
 int len = 0;
 int p;
-char rev;
+char rev
 
 while (s[len] != '\0')
 {
 len++;
 }
-for (p = 0; (p < len / 2); p++)
+for (p = 0; p < len / 2; p++) {
 {
 rev = s[p];
+s[p] = s[len - 1 - p];
+s[len - 1 - p] = rev;
 }
 }
