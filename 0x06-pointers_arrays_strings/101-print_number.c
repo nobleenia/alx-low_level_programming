01d79ab3455fa15eb8cbd73655b92d405ae18e1d
Noble <neluwah@gmail.com>
2023-06-29 12:53:50 -0700

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: input value
+ *
+ * Return: 0
+ */
+void print_number(int n)
+{
+int a = n;
+
+if (n < 0)
+{
+_putchar('-');
+a = -n;
+}
+if (a / 10 != 0)
+{
+print_number(a / 10);
+}
+_putchar((a % 10) + '0');
+}
