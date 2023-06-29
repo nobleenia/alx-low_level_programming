d36165d93f7b654606267913393a2f2c47ffb978
Noble <neluwah@gmail.com>
2023-06-29 14:11:06 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,15 +8,16 @@
  */
 void print_number(int n)
 {
+unsigned int a = n;
 
 if (n < 0)
 {
 _putchar('-');
-n = -n;
+a = -n;
 }
-if (n / 10 != 0)
+if (a / 10 != 0)
 {
-print_number(n / 10);
+print_number(a / 10);
 }
-_putchar((n % 10) + '0');
+_putchar((a % 10) + '0');
 }
