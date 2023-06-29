a4fa5a8a8d8a1f2d0926d69db6e6012ec2566b7f
Noble <neluwah@gmail.com>
2023-06-29 05:44:23 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,15 @@ char *_strncpy(char *dest, char *src, int n)
 int i;
 
 i = 0;
-while (i < n)
+while ((i < n) && (src[i] =! '\0'))
 {
 dest[i] = src[i];
-if (src[i] == '\0')
-{
-break;
-}
 i++;
 }
+while (i < n)
+{
 dest[i] = '\0';
-  
+i++;
+}
 return (dest);
 }
