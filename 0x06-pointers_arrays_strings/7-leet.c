3c171d39158765d269a45d46e177c49b8c8649f6
Noble <neluwah@gmail.com>
2023-06-29 11:45:08 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,24 @@
 #include "main.h"
 
 /**
+ * leet - encodes a string into 1337
+ * @str: the string to be encoded
  *
- *
- *
- *
+ * Return: the encoded string
  */
-char *leet(char *)
+char *leet(char *str)
+{
+int a, b;
+char lee1[] = "aeotlAEOTL";
+char lee2[] = "4307143071";
+
+for (a = 0; str[a] != '\0'; a++)
+{
+for (b = 0; b < 10; b++)
+{
+if (str[a] == lee1[b])
+str[a] = lee2[b];
+}
+}
+return (str);
+}
