0b662705185c0c47fa041d7997282a682cc52933
Noble <neluwah@gmail.com>
2023-07-05 03:37:55 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints the input string followed by a line
+ * @s: the input string
+ * Return: 0 success
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+_putchar(s);
+_puts_recursion(s + 1);
+}
+_putchar('\n');
+
+return (0);
+}
