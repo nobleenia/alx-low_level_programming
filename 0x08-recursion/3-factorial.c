8dbb07c20a51ee6158fe467f90b68c070fd01bfe
Noble <neluwah@gmail.com>
2023-07-05 04:46:20 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - writes the factorial of the input
+ * @n: the input integer
+ *
+ * Return: the factorial product
+ */
+int factorial(int n)
+{
+  if (n < 0)
+    {
+      return (-1);
+    }
+  else if (n == 0)
+    {
+      return (1);
+    }
+  else
+    {
+      return (n * factorial(n - 1));
+    }
+}
