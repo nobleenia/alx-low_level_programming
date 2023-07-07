6d74baed1979f0e51b864f768e9352c1059b33ef
Noble <neluwah@gmail.com>
2023-07-07 13:33:49 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,18 +8,8 @@
  *
  * Return: 0 successful
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-char *name = argv[0];
-char *dlm = name;
-while (*name != '\0')
-{
-if (*name == '/')
-{
-dlm = name + 1;
-}
-name++;
-}
-printf("%s\n", dlm);
+printf("%s\n", argv[argc]);
 return (0);
 }
