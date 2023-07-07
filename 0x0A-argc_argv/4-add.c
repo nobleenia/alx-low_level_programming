be79e4fa3610f6b554dc0b22e4e043a6d144d688
Noble <neluwah@gmail.com>
2023-07-07 15:05:41 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,35 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
+
+int int_chk(char *str);
 
 /**
  * main - sums the integer inputs
  * @argc: arguments counter
  * @argv: arguments vector
  *
- * Return: 0 successful
+ * Return: 0 for successful execution
  */
 int main(int argc, char **argv)
 {
-int n = 0;
-int t;
+
+int n;
+int st_int;
+int i = 0;
+
 if (argc < 2)
 {
 printf("0\n");
+return (0);
 }
-for (t = 1; t < argc; t++)
+
+n = 1;
+while (n < argc)
 {
-int j;
-for (j = 0; argv[t][j] != '\0'; j++)
+if (int_chk(argv[n]))
 {
-if (!isdigit(argv[t][j]))
+st_int = atoi(argv[n]);
+i += st_int;
+}
+else
 {
-printf("Error");
+printf("Error\n");
 return (1);
 }
+n++;
 }
-n += atoi(argv[t]);
-}
-printf("%d\n", n);
+
+printf("%d\n", i);
 return (0);
+}
+
+/**
+ * int_chk - checks if input is a digit or not
+ * @str: the input string
+ *
+ * Return: 1 if the input is a digit, 0 otherwise
+ */
+int int_chk(char *str)
+{
+unsigned int n = 0;
+while (n < strlen(str))
+{
+if (!isdigit(str[n]))
+{
+return (0);
+}
+n++;
+}
+return (1);
 }
