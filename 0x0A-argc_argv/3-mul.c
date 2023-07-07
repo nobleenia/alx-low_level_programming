8102f106f38f0ff562c2e96d41984f93759da0e7
Noble <neluwah@gmail.com>
2023-07-07 14:12:01 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,16 +11,13 @@
 int main(int argc, char **argv)
 {
 
-int a = atoi(argv[1]);
-int b = atoi(argv[2]);
-
 if (argc != 3)
 {
 printf("Error\n");
 return (1);
 }
-int n = a * b;
-printf("%d\n", n);
+
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 return (0);
 }
