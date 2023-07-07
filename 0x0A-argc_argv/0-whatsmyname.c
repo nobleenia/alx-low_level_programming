062c647461b8f3bbae3fddc6442db9d0aa1624f0
Noble <neluwah@gmail.com>
2023-07-07 13:44:38 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
+#define UNUSED(x) (void)(x)
+
 /**
  * main - prints program name followed by a new line
  * @argc: arguments counter
@@ -8,8 +10,10 @@
  *
  * Return: 0 successful
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-printf("%s\n", *argv[argc]);
+
+UNUSED(argc);
+printf("%s\n", argv[0]);
 return (0);
 }
