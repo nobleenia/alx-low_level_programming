5dbf34bf5a4c32305482b85f3ebdfb721acdeab3
Noble <neluwah@gmail.com>
2023-07-07 10:37:08 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,8 @@
  *
  * Return: 0 successful
  */
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
-printf("%s\n", argv[0]);
+printf("%s\n", argv[argc]);
 return (0);
 }
