7941e0e2ffb801a91a11a33519f926fd0eb3aa29
Noble <neluwah@gmail.com>
2023-07-17 05:49:22 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,7 @@
+0x0E. C - Structures, typedef
+0. dog.h: Define a new type struct dog with the elements
+1. 1-init_dog.c: A function that initialize a variable of type struct dog
+2. 2-print_dog.c: A function that prints a struct dog
+3. dog.h: Define a new type dog_t as a new name for the type struct dog
+4. 4-new_dog.c: A function that creates a new dog
+5. 5-free_dog.c A function that frees dogs
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct - structure definition for dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Description: data structure for the dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
