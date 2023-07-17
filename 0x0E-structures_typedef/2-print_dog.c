9f15b77c020181d3fd53cdadf96c3dfcd0c32b7d
Noble <neluwah@gmail.com>
2023-07-17 06:30:10 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,20 +9,37 @@
  */
 void print_dog(struct dog *d)
 {
+
 if (d == NULL)
 {
 return;
 }
+
 if (d->name == NULL)
 {
-printf("Name: (nil)");
+printf("Name: (nil)\n");
 }
+else
+{
+printf("Name: %s\n", d->name);
+}
+
 if (d->age == NULL)
 {
 printf("Age: (nil)");
 }
+else
+{
+printf("Age: %f\n", d->age);
+}
+
 if (d->owner == NULL)
 {
 printf("Owner: (nil)");
 }
+else
+{
+printf("Owner: %s\n", d->owner);
+}
+
 }
