050f837472d6860e69b683a5518c5cbefb7a4196
Noble <neluwah@gmail.com>
2023-07-17 02:27:32 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#include "main.h"
+#define _FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) (((x) < (0)) ? -(x) : (x))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#include "main.h"
+#define _SUM_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
