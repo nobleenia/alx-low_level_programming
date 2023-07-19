39e0d19ad70bf07a4369a5c8d35e10b5c57f8751
Noble <neluwah@gmail.com>
2023-07-19 15:36:55 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program starting point
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0 succesful
+ */
+int main(int argc, char **argv)
+{
+
+int bytes, n;
+unsigned char *main_func;
+
+if (argc != 2)
+{
+printf("Error\n")
+return(1);
+}
+
+bytes = atoi(argv[1]);
+if (bytes < 0)
+{
+printf("Error\n");
+return (2);
+}
+
+main_func = (unsigned char *)main;
+for (n = 0; n < bytes; n++)
+{
+printf("%02x", main_func[n]);
+if (n < bytes - 1)
+{
+printf(" ");
+}
+}
+printf("\n");
+
+return (0);
+}
