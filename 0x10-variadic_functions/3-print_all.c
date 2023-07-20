5972ca090e6a28b7f76525d3b8d562715728901f
Noble <neluwah@gmail.com>
2023-07-20 08:42:09 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -24,22 +24,15 @@ while (format && format[n])
 switch (format[n])
 {
 case 'c':
-{
 printf("%c", va_arg(inputs, int));
 break;
-}
 case 'i':
-{
 printf("%d", va_arg(inputs, int));
 break;
-}
 case 'f':
-{
 printf("%f", va_arg(inputs, double));
 break;
-}
 case 's':
-{
 str = va_arg(inputs, char *);
 if (str == NULL)
 {
@@ -47,13 +40,10 @@ str = "(nil)";
 }
 printf("%s", str);
 break;
-}
 
 default:
-{
-space = ' ';
+space = 0;
 break;
-}
 }
 
 if (format[n + 1] && space)
