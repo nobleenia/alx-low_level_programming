34f222f89e54b00d6657b2cf6b6b231fac4971d4
Noble <neluwah@gmail.com>
2023-07-21 05:29:06 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,11 +2,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void print_c(va_list inputs);
-void print_i(va_list inputs);
-void print_f(va_list inputs);
-void print_s(va_list inputs);
-
 /**
  * print_all - prints all characters from the input
  * @format: the types of inputs
@@ -25,76 +20,31 @@ while (format && format[n])
 switch (format[n])
 {
 case 'c':
-print_c(inputs);
+printf("%s%c", sep, va_arg(inputs, int));
 break;
 case 'i':
-print_i(inputs);
+printf("%s%d", sep, va_arg(inputs, int));
 break;
 case 'f':
-print_f(inputs);
+printf("%s%f", sep, va_arg(inputs, double));
 break;
 case 's':
-print_s(inputs);
+{
+
+char *str = va_arg(inputs, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s%s", sep, str);
+}
 break;
 default:
 break;
 }
 
-if (format[n + 1])
-{
-printf("%s", sep);
 sep = ", ";
-}
-
 n++;
 }
 
 va_end(inputs);
 printf("\n");
-}
-
-/**
- * print_c - prints a character
- * @inputs: variadic arguments list
- */
-void print_c(va_list inputs)
-{
-
-printf("%c", va_arg(inputs, int));
-}
-
-/**
- * print_i - prints an integer
- * @inputs: variadic arguments list
- */
-void print_i(va_list inputs)
-{
-
-printf("%d", va_arg(inputs, int));
-}
-
-/**
- * print_f - prints a float
- * @inputs: variadic arguments list
- */
-void print_f(va_list inputs)
-{
-
-printf("%f", va_arg(inputs, double));
-}
-
-/**
- * print_s - prints a string
- * @inputs: variadic arguments list
- */
-void print_s(va_list inputs)
-{
-
-char *str = va_arg(inputs, char *);
-
-if (str == NULL)
-{
-str = "(nil)";
-}
-printf("%s", str);
 }
