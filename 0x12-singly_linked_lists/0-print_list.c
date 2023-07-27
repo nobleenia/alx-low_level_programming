de1202f8fc0c396d765fcd806b47d489e75ea5b5
Noble <neluwah@gmail.com>
2023-07-27 04:34:27 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,20 +6,22 @@
  * print_list - prints elements in the linked list
  * @h: pointer to the linked list
  *
- * Return: the num of items in the list
+ * Return: the number of items in the list
  */
 size_t print_list(const list_t *h)
 {
-size_t num = 0;
+size_t count = 0;
 
-while (h)
+while (h != NULL)
 {
-if (!h->str)
+if (!h->str == NULL)
 printf("[0] (nil)\n");
 else
 printf("[%u] %s\n", h->len, h->str);
+
 h = h->next;
-num++;
+count++;
+
 }
-return (num);
+return (count);
 }
