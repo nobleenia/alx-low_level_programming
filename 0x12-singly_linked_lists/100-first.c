c0761adc048df1ddc0bdc07fee282876d0e8a84b
Noble <neluwah@gmail.com>
2023-07-27 06:57:24 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,6 @@
  */
 void __attribute__((constructor)) bef_main()
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
