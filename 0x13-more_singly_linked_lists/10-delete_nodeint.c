bcf1d3d33cab658431c7f03df957f0e4e57fa10a
Noble <neluwah@gmail.com>
2023-07-31 12:45:22 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 unsigned int counter = 0;
 listint_t *current = NULL;
-list_int_t *temp = *head;
+listint_t *temp = *head;
 
 if (*head == NULL)
 {
