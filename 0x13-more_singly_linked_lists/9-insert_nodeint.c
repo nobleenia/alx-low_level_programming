82c151583855c5f25b41b00281f46353f63d6c3b
Noble <neluwah@gmail.com>
2023-07-31 12:42:39 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int count;
 listint_t *new_node;
-listint_t *temp;
+listint_t *temp = *head;
 
 new_node = malloc(sizeof(listint_t));
 if (!new_node || !head)
