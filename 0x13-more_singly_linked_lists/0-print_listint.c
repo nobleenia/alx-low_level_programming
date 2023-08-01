9541aa75fa377b9b4e8a3b33a96058fc5a0b986c
Noble <neluwah@gmail.com>
2023-08-01 03:50:54 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,9 +2,9 @@
 
 /**
  * print_listint - prints the items in the linked list
- * @h: pointer to the list
+ * @h: pointer to the linked list
  *
- * Return: the number of items in the list
+ * Return: the number of nodes in the list
  */
 size_t print_listint(const listint_t *h)
 {
@@ -13,8 +13,8 @@ size_t ret_val = 0;
 while (h)
 {
 printf("%d\n", h->n);
-h = h->next;
 ret_val++;
+h = h->next;
 }
 
 return (ret_val);
