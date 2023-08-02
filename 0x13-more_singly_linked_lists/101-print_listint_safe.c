bf2e99fc3287129f95510aea1ce40101f914a0b1
Noble <neluwah@gmail.com>
2023-08-02 02:07:02 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,53 +11,29 @@ const listint_t **re(const listint_t **list, size_t size, const listint_t *new);
 size_t print_listint_safe(const listint_t *head)
 {
 size_t i, num = 0;
-const listint_t **list = NULL;
+const listint_t *slow = head;
+const listint_t *fast = head;
+
+while (fast != NULL && fast->next != NULL)
+{
+num++;
+printf("[%p] %d\n", (void *)slow, slow->n);
+slow = slow->next;
+fast = fast->next;
+
+if (slow == fast)
+{
+printf("->[%p] %d\n", (void *)slow, slow->n);
+return (num);
+}
+}
 
 while (head != NULL)
 {
-for (i = 0; i < num; i++)
-{
-if (head == list[i])
-{
-printf("->[%p] %d\n", (void *)head, head->n);
-free(list);
-return (num);
-}
-}
 num++;
-list = re(list, num, head);
 printf("[%p] %d\n", (void *)head, head->n);
 head = head->next;
 }
-free(list);
+
 return (num);
-}
-
-/**
- * re - reallocates memory for an array of pointers
- * to the nodes in a linked list
- * @list: the old list to append
- * @size: size of the new list
- * @new: new node to add to the list
- *
- * Return: pointer to the new list
- */
-const listint_t **re(const listint_t **list, size_t size, const listint_t *new)
-{
-const listint_t **new_list;
-size_t i;
-
-new_list = malloc(size * sizeof(listint_t));
-if (new_list == NULL)
-{
-free(list);
-exit(98);
-}
-for (i = 0; i < size - 1; i++)
-{
-new_list[i] = list[i];
-}
-new_list[i] = new;
-free(list);
-return (new_list);
 }
