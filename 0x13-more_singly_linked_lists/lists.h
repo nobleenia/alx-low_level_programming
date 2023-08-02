633b6c5fcc49ffc7c9af215bb6d48597289f5643
Noble <neluwah@gmail.com>
2023-08-02 05:30:29 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -35,5 +35,6 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+const listint_t **re(const listint_t **list, size_t size, const listint_t *new);
 
 #endif
