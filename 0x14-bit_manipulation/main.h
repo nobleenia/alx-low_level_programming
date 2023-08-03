95f4661c16ac8e0bb3a597e47744885de1588f3b
Noble <neluwah@gmail.com>
2023-08-03 15:54:33 -0700

    101-password

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -11,5 +13,7 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
+
+#define __home inline
 
 #endif
