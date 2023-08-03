68f922e39bdbbb0d3efea19e4464f0b53e822dde
Noble <neluwah@gmail.com>
2023-08-03 05:44:16 -0700

    Correction for task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,7 +8,7 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int n = 0;
+int n = 0;
 unsigned int dec = 0;
 
 if (b == NULL)
@@ -17,12 +17,16 @@ return (0);
 }
 while (b[n] != '\0')
 {
-if (b[n] != '0' && b[n] != '1')
+if (b[n] == '0' || b[n] == '1')
+{
+dec = dec << 1;
+dec += b[n] - '0';
+n++;
+}
+else
 {
 return (0);
 }
-dec = dec * 2 + (b[n] - 0);
-n++;
 }
 return (dec);
 }
