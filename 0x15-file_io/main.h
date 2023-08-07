888091ecbe1f28ec1430a60a92c2b423eb99da5b
Noble <neluwah@gmail.com>
2023-08-07 10:06:26 -0700

    3-cp.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,10 +6,18 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error(int code, const char *message);
+int open_src_file(const char *file);
+int open_dst_file(const char *file);
+void copy_file(int src_file, int dst_file);
+void close_file(int file);
 
 #endif
