4a69cdfbff7db86ff65256f10a99801546008af0
Noble <neluwah@gmail.com>
2023-08-07 12:22:39 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -23,6 +23,29 @@ void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
 
 /**
+ * check_elf - Checks if a file is an ELF file.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
+ *
+ * Description: If the file is not an ELF file - exit code 98.
+ */
+void check_elf(unsigned char *e_ident)
+{
+int index;
+
+for (index = 0; index < 4; index++)
+{
+if (e_ident[index] != 127 &&
+e_ident[index] != 'E' &&
+e_ident[index] != 'L' &&
+e_ident[index] != 'F')
+{
+dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+exit(98);
+}
+}
+}
+
+/**
  * print_magic - Prints the magic numbers of an ELF header.
  * @e_ident: A pointer to an array containing the ELF magic numbers.
  *
