94ee7e9969ec9b1e365955947445b5135601dd4a
Noble <neluwah@gmail.com>
2023-08-07 06:43:23 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * create_file -  creates a file
+ * @filename: name of the file to create
+ * @text_content: string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+int file;
+ssize_t let_len;
+ssize_t let_print;
+
+if (!filename)
+{
+return (-1);
+}
+
+file = open(filename, O_CREAT | O_RDONLY | O_TRUNC, 0600);
+
+if (file == -1)
+{
+return (-1);
+}
+
+if (!text_content)
+{
+text_content = "";
+}
+
+for (let_len = 0; text_content[let_len]; let_len++)
+{
+}
+
+let_print = write(file, text_content, let_len);
+if (let_len == -1)
+{
+return (-1);
+}
+
+close(file)
+return (1);
+}
