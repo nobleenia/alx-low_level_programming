acd4b9f393bb611206277d19cf90471c0ae4d63c
Noble <neluwah@gmail.com>
2023-09-14 09:57:33 -0700

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,7 +12,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 dlistint_t *current;
 unsigned int counter;
 
-if (*head == NULL)
+if (head == NULL || *head == NULL)
 {
 return (-1);
 }
