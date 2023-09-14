459465c273191f432da48be794e03273625af3df
Noble <neluwah@gmail.com>
2023-09-14 09:34:25 -0700

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -3,7 +3,7 @@
 /**
  * add_dnodeint - adds a new node at the beginning of a dlistint_t list
  * @head: pointer to the pointer of the first node in the list
- * the value of the new_node to be added
+ * @n: the value of the new_node to be added
  *
  * Return: the added node
  */
