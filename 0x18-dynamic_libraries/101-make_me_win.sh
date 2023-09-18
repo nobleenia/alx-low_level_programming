884f31b1be9f406c43a6fcebb256ff17bde7bac0
Noble <neluwah@gmail.com>
2023-09-18 08:43:44 -0700

    Update: Advanced Task 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qP /tmp/alx-low_level_programming/0x18-dynamic_libraries ./inject_code.so
+wget -qP /tmp/ https://github.com/nobleenia/alx-low_level_programming/raw/master/0x18-dynamic_libraries/inject_code.so
 export LD_PRELOAD="/tmp/inject_code.so"
